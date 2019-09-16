9d0d82218d7e3920400528e80f0cd37a5a5fb98d
FjGonzaR <fj.gonzalez@uniandes.edu.co>
2019-09-16 18:21:38 -0500

    first routes

--- config.js
+++ config.js
@@ -2,5 +2,6 @@ const dotenv = require('dotenv');
 dotenv.config();
 module.exports = {
   databaseUser: process.env.DATABASE_USERNAME,
-  databasePassword: process.env.DATABASE_PASSWORD
+  databasePassword: process.env.DATABASE_PASSWORD,
+  databaseName : process.env.DATABASE_NAME
 };
--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,35 @@
 var express = require('express');
 var router = express.Router();
-const { databaseUser, databasePassword } = require('./config');
+const { databaseUser, databasePassword, databaseName } = require('../config');
 const mongoClient = require("mongodb").MongoClient;
 const uri = "";
 
 
 //connect to mongo db
-mongoClient.connect("", {
+/**
+ * let conn =  mongoClient.connect("", {
   useNewUrlParser : true,
   useUnifiedTopology: true
 })
+ */
 
+
+ /**
+  * Get all the courses
+  */
+ function getCourses(req, res){
+  conn.then(client => {
+    client.db(databaseName).collection("courses").find({})
+    .toArray((err,data)=> {
+      if(err) throw err;
+      res.send(data);
+    })
+  });
+ }
+ //------------------------ROUTES------------------------------------------
+ router.get('/courses',(req,res) => getCourses(req,res));
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
-
 module.exports = router;
