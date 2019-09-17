e25345ee5957e44e335ebab51a77d99f3bca99c7
FjGonzaR <fj.gonzalez@uniandes.edu.co>
2019-09-16 19:10:07 -0500

    Database connection

--- routes/index.js
+++ routes/index.js
@@ -2,16 +2,18 @@ var express = require('express');
 var router = express.Router();
 const { databaseUser, databasePassword, databaseName } = require('../config');
 const mongoClient = require("mongodb").MongoClient;
-const uri = "";
-
+const uri = "mongodb+srv://"+databaseUser+":"+databasePassword+"@cluster0-j6ym9.mongodb.net/test?retryWrites=true&w=majority";
 
 //connect to mongo db
-/**
- * let conn =  mongoClient.connect("", {
+  
+let conn =  mongoClient.connect(uri, {
   useNewUrlParser : true,
   useUnifiedTopology: true
 })
- */
+.catch(error => {
+  console.error(error);
+})
+
 
 
  /**
@@ -26,8 +28,35 @@ const uri = "";
     })
   });
  }
+  /**
+  * Get all the courses
+  */
+ function getCategories(req, res){
+  conn.then(client => {
+    client.db(databaseName).collection("categories").find({})
+    .toArray((err,data)=> {
+      if(err) throw err;
+      res.send(data);
+    })
+  });
+ }
+ /**
+  * Get the courses of a category.
+  */
+ function getCoursesOfCategory(req, res){
+  let category = req.params.categoryName;
+  conn.then(client => {
+    client.db(databaseName).collection("categories").find({"category" : category})
+    .toArray((err,data)=> {
+      if(err) throw err;
+      res.send(data);
+    })
+  });
+ }
+
  //------------------------ROUTES------------------------------------------
- router.get('/courses',(req,res) => getCourses(req,res));
+ router.get('/categories',(req,res) => getCategories(req,res));
+ router.get('/categories/:categoryName',(req,res) => getCoursesOfCategory(req,res));
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
