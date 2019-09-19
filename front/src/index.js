5d6f3416bf7a770c02694c47e01c4bd7788f85b2
FjGonzaR <fj.gonzalez@uniandes.edu.co>
2019-09-19 09:30:45 -0500

    come on

--- front/src/index.js
+++ front/src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import TutoFinder from "./components/tutofinder"
-import './Styles.css';
 
 ReactDOM.render(<TutoFinder />, document.getElementById("root"));
