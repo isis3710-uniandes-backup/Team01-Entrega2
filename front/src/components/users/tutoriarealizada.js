09201c15edb5afe963e71f9dc32dcb7a34b0c241
DIANY YISEL QUINTERO PEROZO <dy.quintero@uniandes.edu.co>
2019-09-22 11:17:16 -0500

    Obteniendo info del back

---
+++ front/src/components/users/tutoriarealizada.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import {Card} from 'react-bootstrap';
+import Rating from '@material-ui/lab/Rating';
+import '../../styles/tutoriarealizada.css';
+export default class tutoriarealizada extends Component {
+
+    constructor(props){
+        super(props);
+
+        this.state={
+            materia: this.props.value.materias,
+            direccion: this.props.value.direccion,
+            fecha: this.props.value.fecha,
+            duracion: this.props.value.duracion,
+            horaFinal: "",
+            costo: this.props.value.costo,
+            cupos : this.props.value.cuposRestantes
+        };
+    }
+
+    componentDidMount() {
+        let init = this.state.fecha.split("T")[1].split(".")[0];
+        let horaFina = parseInt(init.split(":")[0]);
+        console.log(horaFina);
+        let minutos = init.split(":")[1];
+        let segundos = "00";
+        horaFina += this.state.duracion;
+        init = horaFina;
+        let horaFinalle = "" + init + ":" + minutos + ":" + segundos;
+        this.setState({
+            horaFinal: horaFinalle
+        })
+        console.log(this.state.horaFinal)
+    }
+
+    render() {
+
+        return (           
+                <div>
+                    <Card className="tutorias" >
+                    <Card.Body className="tutoriasBody">
+                        <Card.Title>
+                            <strong>Monitoria de {this.state.materia}</strong>
+                        </Card.Title>
+                        <Card.Subtitle className="text-muted">{this.state.direccion}</Card.Subtitle>
+                        <h5 className="fecha">{this.state.fecha.split("T")[0]}</h5>
+                        <div className="sameLine">
+                        <h5 className="hora">{this.state.fecha.split("T")[1].split(".")[0]} a {this.state.horaFinal}</h5>
+                        <h5 className="costo" >${this.state.costo}</h5>
+                        <h5 className="cupos" >Cupos actuales : {this.state.cupos}</h5>
+                        </div>
+                        <div className="stars">
+                            <Rating value={3} readOnly />
+                        </div>
+                    </Card.Body>
+                </Card>
+                </div>
+           
+
+        )
+    }
+}
--- front/src/components/users/userinfo.js
+++ front/src/components/users/userinfo.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { Col, Row, Button, Nav, Image,Form} from 'react-bootstrap';
 import '../../styles/userinfo.css';
+import userimage from '../../imgs/usr.png';
+import TutoriaRealizada from './tutoriarealizada';
 
 
 export default class userinfo extends Component {
@@ -9,19 +11,71 @@ export default class userinfo extends Component {
         super(props);
 
         this.state={
-            usuario:"",
-            nombre:"",
-            apellidos:"",
-            contrasenna:"",
-            telefono: "",
-            email:"",
-            cedula:"",
-            img: "https://www.pngfind.com/pngs/m/113-1133633_attribution-icon-person-icon-male-icon-icono-persona.png"
+            estado: "mostrarForm",
+            usuario:this.props.usuario,
+            nombre:this.props.nombre,
+            apellidos:this.props.apellidos,
+            contrasenna:this.props.contrasenna,
+            telefono: this.props.telefono,
+            email:this.props.email,
+            cedula: this.props.cedula,
+            monitoriasRealizadas:[],
+            img: userimage
         };
     }
 
     componentDidMount() {
-        
+        fetch('https://radiant-hollows-88985.herokuapp.com/users/a')
+            .then(res => res.json())
+            .then(json => {
+                let data = json[0];
+                let user = data.usuario;
+                let name = data.nombre;
+                let lastName = data.apellidos;
+                let contra = data.contrasenna;
+                let telf = data.telefono;
+                let correo = data.email;
+                let ced = data.cedula;
+                let idsMonitorias = data.monitoriasRealizadas;
+                let tempMonitorias = this.state.monitoriasRealizadas;
+                for (let index = 0; index < idsMonitorias.length; index++) 
+                {
+                    let monit = idsMonitorias[index];
+                    fetch('https://radiant-hollows-88985.herokuapp.com/monitorias/'+monit)
+                    .then(res => res.json())
+                    .then(json => {
+                        tempMonitorias.push(json[0]);
+                    })   
+                }
+                this.setState({
+                    usuario:user, 
+                    nombre:name, 
+                    apellidos:lastName, 
+                    contrasenna:contra, 
+                    telefono:telf, 
+                    email:correo, 
+                    cedula:ced,
+                    monitoriasRealizadas:tempMonitorias
+                })
+
+            }
+            );
+    }
+
+    putRequest=()=>{
+
+    }
+
+    mostrarTutorias=()=>{
+        this.setState(
+            {estado:"mostrarTutorias"}
+        )
+    }
+
+    mostrarForm=()=>{
+        this.setState(
+            {estado:"mostrarForm"}
+        )
     }
 
     handleNombre = (e)=>{
@@ -44,87 +98,123 @@ export default class userinfo extends Component {
         this.setState({cedula: e.target.value })
     };
 
-    mostrarTutorias = ()=>{
-        
-    };
-
     render() {
-        return (
-            
-                <div id="contenedor">
-                    <Row>
-                        <Col  lg={3} md={5} sm={5} id ="panelOpciones">
-                        <br></br>
-                            <Nav  id="sidebar">
-                                <div className="sidebar-header">
-                                    <Image id="userImg" src={this.state.img} roundedCircle></Image>
-                                    <br></br>
-                                    <br></br>
-                                    <h2 id="name" >{this.state.usuario}</h2>
-                                </div>
-                                
-                                <ul className="list-unstyled components">
-                                    <li className="active">
-                                        <a>Ajustes de cuenta</a>
-                                    </li>
-                                    <li>
-                                        <a onClick={this.mostrarTutorias} >Mis tutorías</a>
-                                    </li>
-                                    <li>
-                                        <a >Cerrar sesión</a>
-                                    </li>
-                                </ul> 
-                            </Nav>
-                        </Col>
-                        
-                        <Col lg={8} md={5} sm={5} id="informacion">
-                        <div id="form">  
-                            <h2>Información de tu cuenta</h2>
-                                <Form>
-                                    <Form.Group>
-                                        <Form.Label>Usuario</Form.Label>
-                                        <Form.Control placeholder="Ingrese el título"
-                                        onChange = {this.handleUser}
-                                        value ={this.state.usuario} />
-                                    </Form.Group>
-                                    <Form.Group>
-                                        <Form.Label>Nombre</Form.Label>
-                                        <Form.Control placeholder="Ingrese el título"
-                                        onChange = {this.handleUser}
-                                        value ={this.state.nombre} />
-                                    </Form.Group>
-                                    <Form.Group>
-                                        <Form.Label>Apellidos</Form.Label>
-                                        <Form.Control placeholder="Ingrese el título"
-                                        onChange = {this.handleApellido}
-                                        value ={this.state.apellidos} />
-                                    </Form.Group>
-                                    <Form.Group >
-                                        <Form.Label>Correo</Form.Label>
-                                        <Form.Control placeholder="Ingrese la descripción"
-                                        onChange = {this.handleEmail}
-                                        value ={this.state.email} />
-                                    </Form.Group>
-                                    <Form.Group >
-                                        <Form.Label>Telefono</Form.Label>
-                                        <Form.Control placeholder="Ingrese la descripción"
-                                        onChange = {this.handleTelefono}
-                                        value ={this.state.telefono} />
-                                    </Form.Group>
-                                    <Form.Group >
-                                        <Form.Label>Cedula</Form.Label>
-                                        <Form.Control placeholder="Ingrese la descripción"
-                                        onChange = {this.handleCedula}
-                                        value ={this.state.cedula} />
-                                    </Form.Group>
-                                    <Button id="actualizar">Actualizar</Button>
-                                </Form>
-                        </div>  
-                        </Col>
-                    </Row>
-                </div>
-           
 
+        if(this.state.estado ==="mostrarForm")
+        {
+            return (
+                <div id="contenedor">
+                        <Row>
+                            <Col  lg={3} md={5} sm={5} id ="panelOpciones">
+                            <br></br>
+                                <Nav  id="sidebar">
+                                    <div className="sidebar-header">
+                                        <Image id="userImg" src={this.state.img} roundedCircle></Image>
+                                        <br></br>
+                                        <br></br>
+                                        <h2 id="name" >{this.state.usuario}</h2>
+                                    </div>
+                                    
+                                    <ul className="list-unstyled components">
+                                        <li>
+                                            <a onClick={this.mostrarForm}>Ajustes de cuenta</a>
+                                        </li>
+                                        <li>
+                                            <a onClick={this.mostrarTutorias} >Mis tutorías</a>
+                                        </li>
+                                        <li>
+                                            <a >Cerrar sesión</a>
+                                        </li>
+                                    </ul> 
+                                </Nav>
+                            </Col>
+                            
+                            <Col lg={8} md={5} sm={5} id="informacion">
+                            <div id="form">  
+                                <h1>Información de tu cuenta</h1>
+                                    <Form>
+                                        <Form.Group>
+                                            <Form.Label>Usuario</Form.Label>
+                                            <Form.Control
+                                            onChange = {this.handleUser}
+                                            value ={this.state.usuario} />
+                                        </Form.Group>
+                                        <Form.Group>
+                                            <Form.Label>Nombre</Form.Label>
+                                            <Form.Control 
+                                            onChange = {this.handleUser}
+                                            value ={this.state.nombre} />
+                                        </Form.Group>
+                                        <Form.Group>
+                                            <Form.Label>Apellidos</Form.Label>
+                                            <Form.Control 
+                                            onChange = {this.handleApellido}
+                                            value ={this.state.apellidos} />
+                                        </Form.Group>
+                                        <Form.Group >
+                                            <Form.Label>Correo</Form.Label>
+                                            <Form.Control 
+                                            onChange = {this.handleEmail}
+                                            value ={this.state.email} />
+                                        </Form.Group>
+                                        <Form.Group >
+                                            <Form.Label>Telefono</Form.Label>
+                                            <Form.Control 
+                                            onChange = {this.handleTelefono}
+                                            value ={this.state.telefono} />
+                                        </Form.Group>
+                                        <Form.Group >
+                                            <Form.Label>Cedula</Form.Label>
+                                            <Form.Control 
+                                            onChange = {this.handleCedula}
+                                            value ={this.state.cedula} />
+                                        </Form.Group>
+                                        <Button id="actualizar">Actualizar</Button>
+                                    </Form>
+                            </div>  
+                            </Col>
+                        </Row>
+                    </div>
+            )
+        }
+    else{
+        
+        return(
+            <div id="contenedor">
+                        <Row>
+                            <Col  lg={3} md={5} sm={5} id ="panelOpciones">
+                            <br></br>
+                                <Nav  id="sidebar">
+                                    <div className="sidebar-header">
+                                        <Image id="userImg" src={this.state.img} roundedCircle></Image>
+                                        <br></br>
+                                        <br></br>
+                                        <h2 id="name" >{this.state.usuario}</h2>
+                                    </div>
+                                    
+                                    <ul className="list-unstyled components">
+                                        <li>
+                                            <a onClick={this.mostrarForm}>Ajustes de cuenta</a>
+                                        </li>
+                                        <li>
+                                            <a onClick={this.mostrarTutorias} >Mis tutorías</a>
+                                        </li>
+                                        <li>
+                                            <a >Cerrar sesión</a>
+                                        </li>
+                                    </ul> 
+                                </Nav>
+                            </Col>
+                            
+                            <Col lg={8} md={5} sm={5} id="informacion">
+                            <div id="form">  
+                                <h1>Mis tutorías</h1>
+                                {this.state.monitoriasRealizadas.map((e,i) => <TutoriaRealizada key={i} value={e}/>)}
+                            </div>  
+                            </Col>
+                        </Row>
+                    </div>
         )
+    }
     }
 }
